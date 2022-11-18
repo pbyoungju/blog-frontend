2305a2d955698c4d15bfc7e6672321b1dfec4d4f
Byoungju Park <pbyoungju@icloud.com>
2022-11-19 01:58:35 +0900

    init commit

--- src/main/kotlin/com/pbyoungju/blog/frontend/Application.kt
+++ src/main/kotlin/com/pbyoungju/blog/frontend/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class BlogFrontendApplication
+class Application
 
 fun main(args: Array<String>) {
-    runApplication<BlogFrontendApplication>(*args)
+    runApplication<Application>(*args)
 }
--- src/main/kotlin/com/pbyoungju/blog/frontend/adapters/enter/page/IndexController.kt
+++ src/main/kotlin/com/pbyoungju/blog/frontend/adapters/enter/page/IndexController.kt
@@ -1,4 +1,13 @@
 package com.pbyoungju.blog.frontend.adapters.enter.page
 
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
 class IndexController {
+
+    @GetMapping("/")
+    fun index(): String {
+        return "index"
+    }
 }
